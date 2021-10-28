8392a60285336f79179f76c35e5b36b907a7ddea
JYPark09 <jyp10987@gmail.com>
2021-10-28 11:08:22 +0900

    hw06 문서 추가

--- hw06/draw_hist.py
+++ hw06/draw_hist.py
@@ -6,13 +6,14 @@ import numpy as np
 
 if __name__ == '__main__':
   if len(sys.argv) != 3:
-    print('usage: python3 {} <output file> <bins>'.format(sys.argv[0]))
+    print('usage: python3 {} <output file> <title>'.format(sys.argv[0]))
     sys.exit(1)
 
   output = sys.argv[1]
-  bins = int(sys.argv[2])
+  title = sys.argv[2]
 
   values = np.fromiter(map(float, input().split()), dtype=np.float)
 
-  plt.hist(values, bins=bins)
+  plt.hist(values, bins=100)
+  plt.title(title)
   plt.savefig(output)
